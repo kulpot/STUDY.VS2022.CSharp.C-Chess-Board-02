aee0d5315f08050a716b8f9355a0710d0dec56a2
Kulpot <kulpotwahu@gmail.com>
2024-02-25 21:49:45 +0800

    https://www.youtube.com/watch?v=SFMVyiJ2S6g&list=PLhPyEFL5u-i0YDRW6FLMd1PavZp9RcYdF&index=3

--- C# Chess Board 02/Program.cs
+++ C# Chess Board 02/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//-------------------- C# Chess Board 02 board cell classes -------------------------------
+//ref link:https://www.youtube.com/watch?v=SFMVyiJ2S6g&list=PLhPyEFL5u-i0YDRW6FLMd1PavZp9RcYdF&index=3
+
 namespace C__Chess_Board_02
 {
     internal class Program
