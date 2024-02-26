3197c8f3065679a4ec8c08f35251f99fd0017278
Kulpot <kulpotwahu@gmail.com>
2024-02-26 19:44:08 +0800

    added constructor name Board to Board.cs

--- ClassBoardModel/Board.cs
+++ ClassBoardModel/Board.cs
@@ -14,6 +14,21 @@ namespace ClassBoardModel
 
         // 2d array of type cell.
         public Cell[,] theGrid { get; set; }
+
+        public Board(int s)
+        {
+            Size = s;
+
+            theGrid = new Cell[Size, Size];
+
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    theGrid
+                }
+            }
+        }
         //---------END--------- C# Chess Board 02 board cell classes -------------------------------
 
     }
