f2b7db97cbc3d246358207f5705fd2401fc4adb3
Kulpot <kulpotwahu@gmail.com>
2024-02-26 19:41:10 +0800

    add prop to Board.cs: // the size of the board usually 8x8 public int Size { get; set; }
    // 2d array of type cell.
    public Cell[,] theGrid { get; set; }

--- ClassBoardModel/Board.cs
+++ ClassBoardModel/Board.cs
@@ -8,5 +8,13 @@ namespace ClassBoardModel
 {
     class Board
     {
+        //---------START--------- C# Chess Board 02 board cell classes -------------------------------
+        // the size of the board usually 8x8
+        public int Size { get; set; }
+
+        // 2d array of type cell.
+        public Cell[,] theGrid { get; set; }
+        //---------END--------- C# Chess Board 02 board cell classes -------------------------------
+
     }
 }
